4ba6964936e4c2f5f6d14f0a4972fd865d957f84
playfulfence <konnor1980@yandex.ru>
2024-02-18 04:03:15 +0100

    chip initialization function -> macro

--- src/lib.rs
+++ src/lib.rs
@@ -16,34 +16,38 @@ use esp32s3_hal as hal;
 
 use hal::{clock::*, gpio::*, prelude::*};
 
-// Define a generic initialization function that is dependent on a feature flag for the chip
-pub fn initialize_chip() -> Result<ChipConfig, InitError> {
-    let peripherals = hal::peripherals::Peripherals::take();
-    let system = peripherals.SYSTEM.split();
-    let clocks = hal::clock::ClockControl::boot_defaults(system.clock_control).freeze();
+macro_rules! initialize_peripherals {
+    ($peripherals:expr) => {{
+        // Use the peripherals to set up the system, clocks, GPIO, etc.
+        // This assumes that the `split` and initialization methods only borrow from peripherals
+        let system_parts = $peripherals.SYSTEM.split();
+        let clocks = ClockControl::boot_defaults(&system_parts.clock_control).freeze();
 
-    let io = IO::new(peripherals.GPIO, peripherals.IO_MUX);
+        let io = IO::new($peripherals.GPIO, $peripherals.IO_MUX);
 
-    Ok(ChipConfig {
-        clocks,
-        gpio: io.pins,
-        peripheral_access: peripherals,
-    })
+        // Return a tuple or a struct that contains the initialized components
+        // and a reference to the peripherals for further use
+        Ok(ChipConfig {
+            clocks,
+            gpio: io.pins,
+            $peripherals,
+        })
+    }};
 }
 
-// Define what the `ChipConfig` might look like
-pub struct ChipConfig {
+// Define the `ChipConfig`
+pub struct ChipConfig<'a> {
     // The fields here represent the peripherals that have been initialized
     pub clocks: hal::clock::Clocks<'static>,
     pub gpio: hal::gpio::Pins,
-    peripheral_access: hal::peripherals::Peripherals,
+    pub periph: &'a hal::peripherals::Peripherals,
 }
 
-// impl ChipConfig {
-//     pub fn get_peripheral(&self) -> hal::peripherals::Peripherals {
-//         self.peripheral_access;
-//     }
-// }
+impl ChipConfig<'_> {
+    pub fn get_peripheral(&self) -> &hal::peripherals::Peripherals {
+        self.periph
+    }
+}
 
 // Define an error type for initialization errors
 pub enum InitError {
