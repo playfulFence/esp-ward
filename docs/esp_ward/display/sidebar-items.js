a94255bcfbc08b97c9df99902bd800d345637e36
playfulFence <62840029+playfulFence@users.noreply.github.com>
2024-04-06 16:51:12 +0000

    Deploying to gh-pages from @ playfulFence/esp-ward@e2d9446513ed7bc69e1f7db163e17000cbad0627 🚀

--- docs/esp_ward/display/sidebar-items.js
+++ docs/esp_ward/display/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"enum":["DisplaySegment"],"mod":["ili9341","max7219","pcd8544"],"trait":["Display","EGDisplay"]};
+window.SIDEBAR_ITEMS = {"enum":["DisplaySegment"],"mod":["ili9341","pcd8544"],"trait":["Display","EGDisplay"]};
--- docs/src-files.js
+++ docs/src-files.js
@@ -1,4 +1,4 @@
 var srcIndex = new Map(JSON.parse('[\
-["esp_ward",["",[["display",[],["ili9341.rs","max7219.rs","mod.rs","pcd8544.rs"]],["peripherals",[],["aht20.rs","bme280.rs","button.rs","joystick.rs","mod.rs","pir.rs","sgp30.rs","tsl2591.rs","ultrasonic_distance.rs"]]],["connectivity.rs","lib.rs"]]]\
+["esp_ward",["",[["display",[],["ili9341.rs","mod.rs","pcd8544.rs"]],["peripherals",[],["aht20.rs","bme280.rs","button.rs","joystick.rs","mod.rs","pir.rs","sgp30.rs","tsl2591.rs","ultrasonic_distance.rs"]]],["connectivity.rs","lib.rs"]]]\
 ]'));
 createSrcSidebar();
